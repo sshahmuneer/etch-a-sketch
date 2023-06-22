d1f1b67f0691dd9aadf0f4e192b78e18f22736f4
Sayed Shah <133984128+sshahmuneer@users.noreply.github.com>
2023-06-22 18:23:28 -0400

    Add slider to select the number of squares per side of the grid

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 const gridContainer = document.querySelector('.grid-container');
-
-for (let i = 0; i < 256; i++) {
-    const gridSquare = document.createElement('div');
-    gridSquare.classList.toggle('grid-square');
-    gridSquare.style.width = '60px';
-    gridSquare.style.height = '60px';
-    // gridSquare.style.backgroundColor = 'yellow';
-
-
-    gridContainer.appendChild(gridSquare);
-}
+const slider = document.querySelector('.slider');
+const sliderValue = document.querySelector('.slider-value');
 
 gridContainer.addEventListener('mouseover', (event) => {
     event.target.style.backgroundColor = 'blue';
 })
+
+slider.addEventListener('input', (event) => {
+    sliderValue.textContent = `Slider value is: ${slider.value}`;
+    createGrid(+slider.value);
+})
+
+function createGrid(numberOfSquaresPerSide) {
+    gridContainer.replaceChildren();   
+    const totalNumberOfSquares = numberOfSquaresPerSide * numberOfSquaresPerSide;
+
+    for (let i = 0; i < totalNumberOfSquares; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.toggle('grid-square');
+        gridSquare.style.width = `${960/numberOfSquaresPerSide}px`;
+        gridSquare.style.height = `${960/numberOfSquaresPerSide}px`;    
+        gridContainer.appendChild(gridSquare);
+    }
+}
+
+sliderValue.textContent = `Slider value is: ${slider.value}`;
+createGrid(+slider.value);
