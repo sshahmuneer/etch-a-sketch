ec76ae39a38cbb80005114382a01242a7b168931
Sayed Shah <133984128+sshahmuneer@users.noreply.github.com>
2023-06-22 16:17:41 -0400

    Create grid container with 256 square grids inside

---
+++ script.js
@@ -0,0 +1,12 @@
+const gridContainer = document.querySelector('.grid-container');
+
+for (let i = 0; i < 256; i++) {
+    const gridSquare = document.createElement('div');
+    gridSquare.classList.toggle('grid-square');
+    gridSquare.style.width = '60px';
+    gridSquare.style.height = '60px';
+    // gridSquare.style.backgroundColor = 'yellow';
+
+
+    gridContainer.appendChild(gridSquare);
+}
